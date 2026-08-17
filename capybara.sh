fa1d01c213228be845d6eedce3ddace35453bf0a
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2026-08-17 09:06:58 +0000

    Make capybara.sh self-contained

--- capybara.sh
+++ capybara.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 set -Eeu -o pipefail
 
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-readonly SCRIPT_DIR
-# shellcheck disable=SC1091
-. "$SCRIPT_DIR"/tools/colored_echo.sh
-# shellcheck disable=SC1091
-. "$SCRIPT_DIR"/tools/container_engine.sh
-
-CONTAINER_ENGINE=$(detect_container_engine)
-readonly CONTAINER_ENGINE
-if [[ $CONTAINER_ENGINE == docker ]]; then
-  ENGINE_OPTS=(-u "$(id -u):$(id -g)")
+if command -v docker &>/dev/null; then
+  docker container run \
+    --name "capybara_$(uuidgen | head -c8)" \
+    --net "${NETWORK:-bridge}" \
+    --rm \
+    -t \
+    -u "$(id -u):$(id -g)" \
+    -v "$PWD":/work:ro \
+    ghcr.io/shakiyam/capybara "$@"
+elif command -v podman &>/dev/null; then
+  podman container run \
+    --name "capybara_$(uuidgen | head -c8)" \
+    --net "${NETWORK:-bridge}" \
+    --rm \
+    --security-opt label=disable \
+    -t \
+    -v "$PWD":/work:ro \
+    ghcr.io/shakiyam/capybara "$@"
 else
-  ENGINE_OPTS=(--security-opt label=disable)
+  echo 'Neither docker nor podman is installed.' >&2
+  exit 1
 fi
-readonly ENGINE_OPTS
-
-$CONTAINER_ENGINE container run \
-  --name "capybara_$(uuidgen | head -c8)" \
-  --net "${NETWORK:-bridge}" \
-  --rm \
-  -t \
-  "${ENGINE_OPTS[@]}" \
-  -v "$PWD":/work:ro \
-  ghcr.io/shakiyam/capybara "$@"
